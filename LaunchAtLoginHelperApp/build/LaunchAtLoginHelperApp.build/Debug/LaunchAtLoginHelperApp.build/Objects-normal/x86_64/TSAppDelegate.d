857d80b5807e5dc9d9fba399c2629088e95960fa
Jacob Rosenthal <jakerosenthal@gmail.com>
2013-08-27 08:48:49 -0400

    removing more build directories

--- LaunchAtLoginHelperApp/build/LaunchAtLoginHelperApp.build/Debug/LaunchAtLoginHelperApp.build/Objects-normal/x86_64/TSAppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/jacobrosenthal/Desktop/memoryio-macosx/LaunchAtLoginHelperApp/LaunchAtLoginHelperApp/TSAppDelegate.m \
-  /Users/jacobrosenthal/Desktop/memoryio-macosx/LaunchAtLoginHelperApp/LaunchAtLoginHelperApp/TSAppDelegate.h
--- LaunchAtLoginHelperApp/build/LaunchAtLoginHelperApp.build/Debug/LaunchAtLoginHelperApp.build/Objects-normal/x86_64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/jacobrosenthal/Desktop/memoryio-macosx/LaunchAtLoginHelperApp/LaunchAtLoginHelperApp/main.m
